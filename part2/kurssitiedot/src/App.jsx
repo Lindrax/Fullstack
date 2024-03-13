6161ce570280489f11f25ba80e17ad2c63314225
Axel <->
2024-03-13 13:03:48 +0200

    2.4

--- part2/kurssitiedot/src/App.jsx
+++ part2/kurssitiedot/src/App.jsx
@@ -1,49 +1,87 @@
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
     const Course = ({course}) => {
       console.log(course)
       const Header = ({ course }) => <h1>{course}</h1>
 
       const Content = ({ parts }) => {
+        
         return (
           <div>
             {parts.map(part => <p>{part.name} {part.exercises}</p>)} 
           </div>
         )
         }
-          
+      const Total = ({parts}) => {
+        
+        const sum = parts.reduce(
+          (total, currentvalue) => {
+            return(total += currentvalue.exercises)
+          }, 0)
+        return (
+          <b> total of {sum} exercises</b>
+        )
+      }
+      
         return(
           <div>
             <Header course={course.name}/>
             <Content parts={course.parts}/>
+            <Total parts={course.parts}/>
           </div>
         )
         }
         
   return (
     <div>
-      <Course course={course} />
+        {courses.map(course =>
+          <Course course={course}/>
+          )}
     </div>
   )
 }
