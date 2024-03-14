66ce2ea0654bfdb23e342f0f6f6ef5337653fc55
Axel <->
2024-03-14 18:14:19 +0200

    2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,11 +2,27 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number:'0001' },
+    { name: 'Ada Lovelace', number: '39-44-5323523'},
+    { name: 'Dan Abramov', number: '12-43-234345'},
+    { name: 'Mary Poppendieck', number: '39-23-6423122'}
   ]) 
   const [newName, setNewName] = useState(
     ''
     )
+  const [newNumber, setNumber] = useState(
+    ''
+  )
+  const [show, setShow] = useState(true)
+
+  const [filt ,setFilt] =useState(
+    ''
+  )
+
+  const ToShow = show
+  ? persons
+  : persons.filter(person => person.name.toLowerCase().includes(filt.toLowerCase()))
+
   const addName = (event) =>{
     event.preventDefault()
     let d=0
@@ -18,7 +34,8 @@ const App = () => {
   })
 
     const newPerson = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
 
     if (d==1){
@@ -26,20 +43,41 @@ const App = () => {
     }
     else{
       setPersons(persons.concat(newPerson))
+      
     }
     setNewName('')
+    setNumber('')
   }
 
   const handleNameChange=(event) => {
     setNewName(event.target.value)
   }
 
+  const handeNumberChange=(event) => {
+    console.log(event.target.value)
+    setNumber(event.target.value)
+  }
+
+  const handleFilt=(event) => {
+    setShow(false)
+    console.log(event.target.value)
+    setFilt(event.target.value)
+  }
+
+
+
   return (
     <div>
       <h2>Phonebook</h2>
+      
+      Filter shown with <input value={filt} onChange={handleFilt}/>
+      <h2>add a new</h2>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handeNumberChange}/>
         </div>
         <div>
           <button type="submit">add</button>
@@ -47,9 +85,9 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       
-        {persons.map(person =>
-          <p key={person.name}> {person.name}</p>
-          )}
+      {ToShow.map(person =>
+        <p key={person.name}> {person.name} {person.number}</p>
+      )}
       
       ...
     </div>
