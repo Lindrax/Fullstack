5c88ca8f27736e80913e4449357e7cd6339e6a96
Axel <->
2024-03-14 19:14:38 +0200

    2.10

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,40 @@
 import { useState } from 'react'
 
+const Filter=({filt, handleFilt}) =>{
+  return(
+    <div>
+      Filter shown with <input value={filt} onChange={handleFilt}/>
+    </div>
+  )
+}
+
+const NewPerson=({newName, newNumber, handeNumberChange, handleNameChange, addName}) =>{
+  return (
+    <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handeNumberChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+
+  )
+}
+
+const Showed=({ToShow})=>{
+  return(
+    <div>
+  {ToShow.map(person =>
+    <p key={person.name}> {person.name} {person.number}</p>
+  )}
+  </div>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number:'0001' },
@@ -20,8 +55,9 @@ const App = () => {
   )
 
   const ToShow = show
-  ? persons
-  : persons.filter(person => person.name.toLowerCase().includes(filt.toLowerCase()))
+    ? persons
+    : persons.filter(person => person.name.toLowerCase().includes(filt.toLowerCase()))
+  
 
   const addName = (event) =>{
     event.preventDefault()
@@ -70,26 +106,18 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
       
-      Filter shown with <input value={filt} onChange={handleFilt}/>
+      <Filter filt={filt} handleFilt={handleFilt} />
       <h2>add a new</h2>
-      <form onSubmit={addName}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange}/>
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handeNumberChange}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <NewPerson
+      newName={newName}
+      newNumber={newNumber}
+      handleNameChange={handleNameChange}
+      handeNumberChange={handeNumberChange}
+      addName={addName}
+      />
       <h2>Numbers</h2>
       
-      {ToShow.map(person =>
-        <p key={person.name}> {person.name} {person.number}</p>
-      )}
-      
-      ...
+      <Showed ToShow={ToShow}/>
     </div>
   )
 }
