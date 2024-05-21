00d29e2e5fea83d3cedf3df5f7b40523a84d3938
Axel <->
2024-05-21 17:48:33 +0300

    4.5

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -9,7 +9,22 @@ const totalLikes =(blogs) => {
       (total, n) => total + n.likes,0,)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null
+  const favorite = blogs.reduce(
+    (accumulator, currentValue) => {
+      return accumulator.likes > currentValue.likes
+        ? accumulator
+        : currentValue
+    }
+  )
+
+  const { title, author, likes } = favorite
+  return { title, author, likes }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
