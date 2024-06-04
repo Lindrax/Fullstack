5b32ce853fbee4455cea02ca09e3c803f7d460e8
Axel <->
2024-06-04 15:34:18 +0300

    4.19

--- part4/app.js
+++ part4/app.js
@@ -5,6 +5,7 @@ require('express-async-errors')
 const cors = require('cors')
 const notesRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
@@ -33,6 +34,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/users', usersRouter)
 app.use('/api/blogs', notesRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
