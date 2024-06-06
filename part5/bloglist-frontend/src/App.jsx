310d9bc3b18d5d2bf082c00ac3e5df93e0ac3da2
Axel <->
2024-06-06 12:55:56 +0300

    5.4

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -2,15 +2,20 @@ import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
-
+import Notification from './components/Notification'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [newBlog, setNewBlog] = useState("")
+  const [title, setTitle] = useState("")
+  const [author, setAuthor] = useState("")
+  const [url, setUrl] = useState("")
   const [username, setUsername] = useState('') 
   const [password, setPassword] = useState('') 
   const [user, setUser] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
-
+  const [errorType, setErrorType] = useState('notification')
+  
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
@@ -35,27 +40,72 @@ const App = () => {
       window.localStorage.setItem(
         'loggedUser', JSON.stringify(user)
       )
-      console.log(window.localStorage)
+      
       blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
+      setErrorType('notification')
+      setErrorMessage('logged in succesfully')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
     } catch (exception) {
       console.log('login error')
+      setErrorType('error')
+      setErrorMessage('wrong username or password')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
     }
+  }
+
+  const addBlog= (event) => {
+    event.preventDefault()
+    const blogObject = {
+      title: title,
+      author: author,
+      url: url
+    }
+  
+    blogService
+      .create(blogObject)
+        .then(returnedBlog => {
+        setBlogs(blogs.concat(returnedBlog))
+        setErrorType('notification')
+        setErrorMessage(`added ${title}, by ${author} succesfully`)
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+        setNewBlog('')
+        setTitle('')
+        setAuthor('')
+        setUrl('')
+      })
+      .catch(error => {
+        setErrorType('error')
+        setErrorMessage('Fill in all the information')
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+      })
+      
   }
 
   const logOut =() => {
     console.log('toimii')
     window.localStorage.clear()
+    setErrorType('notification')
+    setErrorMessage(`logged out succesfully`)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 5000)
     location.reload()
   }
 
-  if (user === null) {
-    return (
-      <div>
-        <h2>Log in to application</h2>
-        <form onSubmit={handleLogin}>
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
         <div>
           username
             <input
@@ -75,7 +125,49 @@ const App = () => {
           />
         </div>
         <button type="submit">login</button>
-      </form>
+      </form> 
+  )
+
+  const handleTitle = (event) => {
+    setTitle(event.target.value)
+  }
+  const handleAuthor = (event) => {
+    setAuthor(event.target.value)
+  }
+  const handleUrl = (event) => {
+    setUrl(event.target.value)
+  }
+
+  const blogForm = () => (
+    <form onSubmit={addBlog}>
+      <div>
+      title:<input
+        value={title}
+        onChange={handleTitle}
+      />
+      </div>
+      <div>
+      author:<input
+        value={author}
+        onChange={handleAuthor}
+      />
+      </div>
+      <div>
+      url:<input
+        value={url}
+        onChange={handleUrl}
+      />
+      </div>
+      <button type="submit">create</button>
+    </form>  
+  )
+  
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        <Notification message={errorMessage} type={errorType}/>
+        {loginForm()}
       </div>
     )
   }
@@ -85,7 +177,9 @@ const App = () => {
       <h2>blogs</h2>
       
       <p>{user.name} is logged in <button onClick={logOut}>logout</button> </p>
-      
+      <h2>create a new blog</h2>
+      <Notification message={errorMessage} type={errorType}/>
+      {blogForm()}
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
