af470cd05d8edc209b41daa3f4d64cf3d40e436d
Axel <->
2024-07-13 13:09:24 +0300

    9.1-3

---
+++ part9/typescript/bmiCalculator.ts
@@ -0,0 +1,41 @@
+const calculateBmi = (height: number, weight: number) => {
+  const ratio = (weight / height / height) * 10000;
+  if (ratio < 18.5) {
+    return "underweight";
+  }
+  if (ratio < 24.9) {
+    return "normal (healthy weight)";
+  }
+  if (ratio < 29.9) {
+    return "overweight";
+  }
+  if (ratio > 30) {
+    return "obese";
+  }
+  return "error";
+};
+
+try {
+  const args = process.argv.slice(2);
+  if (args.length !== 2) {
+    console.error("Please provide exactly two arguments: height and weight");
+    process.exit(1);
+  }
+
+  const height = Number(args[0]);
+  const weight = Number(args[1]);
+
+  if (isNaN(height) || isNaN(weight)) {
+    console.error("Both arguments must be numbers");
+    process.exit(1);
+  }
+  console.log(weight, height);
+  console.log(calculateBmi(180, 74));
+  console.log(calculateBmi(height, weight));
+} catch (error: unknown) {
+  let errorMessage = "Something went wrong: ";
+  if (error instanceof Error) {
+    errorMessage += error.message;
+  }
+  console.log(errorMessage);
+}
---
+++ part9/typescript/exerciseCalculator.ts
@@ -0,0 +1,65 @@
+interface Result {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
+
+const calculateExercises = (hours: number[], target: number): Result => {
+  const average = hours.reduce((s, c) => s + c, 0) / hours.length;
+  const ratio = average / target;
+  let rate;
+  let desc;
+
+  if (ratio >= 1) {
+    rate = 3;
+    desc = "Excellent, target met!";
+  } else if (ratio >= 0.7) {
+    rate = 2;
+    desc = "Not too bad but could be better";
+  } else {
+    rate = 1;
+    desc = "You need to work harder";
+  }
+
+  return {
+    periodLength: hours.length,
+    trainingDays: hours.filter((d) => d > 0).length,
+    success: average > target,
+    rating: rate,
+    ratingDescription: desc,
+    target: target,
+    average: average,
+  };
+};
+
+try {
+  const [, , targetArg, ...exerciseHoursArgs] = process.argv;
+
+  if (!targetArg || exerciseHoursArgs.length === 0) {
+    console.error(
+      "Please provide a target value and at least one exercise hour value."
+    );
+    process.exit(1);
+  }
+
+  const target = Number(targetArg);
+  const exerciseHours = exerciseHoursArgs.map((arg) => Number(arg));
+
+  if (isNaN(target) || exerciseHours.some(isNaN)) {
+    console.error("All values should be numbers.");
+    process.exit(1);
+  }
+
+  console.log(calculateExercises(exerciseHours, target));
+  //console.log(calculateExercises([3, 5, 2, 4.5, 0, 3, 1], 2));
+} catch (error: unknown) {
+  let errorMessage = "Something went wrong: ";
+  if (error instanceof Error) {
+    errorMessage += error.message;
+  }
+  console.log(errorMessage);
+}
