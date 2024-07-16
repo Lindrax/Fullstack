5870792dbdf106288425262c3ad6679f7da1fedf
Axel <->
2024-07-16 11:59:15 +0300

    9.7

--- part9/typescript/exerciseCalculator.ts
+++ part9/typescript/exerciseCalculator.ts
@@ -35,31 +35,33 @@ const calculateExercises = (hours: number[], target: number): Result => {
     average: average,
   };
 };
+if (require.main === module) {
+  try {
+    const [, , targetArg, ...exerciseHoursArgs] = process.argv;
 
-try {
-  const [, , targetArg, ...exerciseHoursArgs] = process.argv;
+    if (!targetArg || exerciseHoursArgs.length === 0) {
+      console.error(
+        "Please provide a target value and at least one exercise hour value."
+      );
+      process.exit(1);
+    }
 
-  if (!targetArg || exerciseHoursArgs.length === 0) {
-    console.error(
-      "Please provide a target value and at least one exercise hour value."
-    );
-    process.exit(1);
+    const target = Number(targetArg);
+    const exerciseHours = exerciseHoursArgs.map((arg) => Number(arg));
+
+    if (isNaN(target) || exerciseHours.some(isNaN)) {
+      console.error("All values should be numbers.");
+      process.exit(1);
+    }
+
+    console.log(calculateExercises(exerciseHours, target));
+    //console.log(calculateExercises([3, 5, 2, 4.5, 0, 3, 1], 2));
+  } catch (error: unknown) {
+    let errorMessage = "Something went wrong: ";
+    if (error instanceof Error) {
+      errorMessage += error.message;
+    }
+    console.log(errorMessage);
   }
-
-  const target = Number(targetArg);
-  const exerciseHours = exerciseHoursArgs.map((arg) => Number(arg));
-
-  if (isNaN(target) || exerciseHours.some(isNaN)) {
-    console.error("All values should be numbers.");
-    process.exit(1);
-  }
-
-  console.log(calculateExercises(exerciseHours, target));
-  //console.log(calculateExercises([3, 5, 2, 4.5, 0, 3, 1], 2));
-} catch (error: unknown) {
-  let errorMessage = "Something went wrong: ";
-  if (error instanceof Error) {
-    errorMessage += error.message;
-  }
-  console.log(errorMessage);
 }
+export default calculateExercises;
--- part9/typescript/index.ts
+++ part9/typescript/index.ts
@@ -1,15 +1,46 @@
 import express from "express";
 const app = express();
 import _calculateBmi from "./bmiCalculator";
+import _calculateExercises from "./exerciseCalculator";
+app.use(express.json());
 
 app.get("/hello", (_req, res) => {
   res.send("Hello Full Stack");
 });
 
 app.get("/bmi", (reg, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   const { height, weight } = reg.query;
+  if (!height || !weight || isNaN(Number(height)) || isNaN(Number(weight))) {
+    return res.status(400).send({ error: "malformatted input" });
+  }
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
   const result = _calculateBmi(Number(height), Number(weight));
-  res.send(result);
+  return res.send(result);
+});
+
+app.post("/exercises", (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { daily_exercises, target } = req.body;
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+  const array = daily_exercises.map(Number);
+  console.log(array);
+  if (!daily_exercises || !target) {
+    return res.status(400).send({ error: "missing parameters" });
+  }
+  const targetNumber = Number(target);
+  if (
+    isNaN(targetNumber) ||
+    !Array.isArray(array) ||
+    array.some((num) => isNaN(Number(num)))
+  ) {
+    return res.status(400).send({ error: "malformatted parameters" });
+  }
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  const result = _calculateExercises(array, targetNumber);
+  console.log(result);
+  return res.send(result);
 });
 
 const PORT = 3003;
